580048108f875ffbfa0641749a09ea7e3b9cea72
Minhazul Abedin <115995410+OkayAbedin@users.noreply.github.com>
2025-04-02 01:20:59 +0600

    added

---
+++ lib/main.dart
@@ -0,0 +1,468 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: LoginScreen());
+  }
+}
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool isUserLogin = true; // Track active tab
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      endDrawer: _buildSidebar(context),
+      body: Builder(
+        builder:
+            (context) => Column(
+              children: [
+                // Purple header section
+                Container(
+                  width: double.infinity,
+                  height: MediaQuery.of(context).size.height * 0.5,
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 88, 13, 218),
+                    borderRadius: const BorderRadius.only(
+                      bottomLeft: Radius.circular(40),
+                    ),
+                  ),
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 40,
+                    vertical: 40,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Align(
+                        alignment: Alignment.topRight,
+                        child: IconButton(
+                          icon: Icon(Icons.menu, color: Colors.white, size: 30),
+                          onPressed: () {
+                            Scaffold.of(context).openEndDrawer();
+                          },
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      Text(
+                        "\nDIU\nROUTE\nEXPLORER",
+                        style: GoogleFonts.inter(
+                          color: Colors.white,
+                          fontSize: 60,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+                // White content section
+                Expanded(
+                  child: Container(
+                    width: double.infinity,
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 30,
+                      vertical: 30,
+                    ),
+                    decoration: const BoxDecoration(color: Colors.white),
+                    child: Column(
+                      children: [
+                        // User/Admin Login Tabs with Underline Indicator
+                        Row(
+                          children: [
+                            Expanded(
+                              child: GestureDetector(
+                                onTap: () {
+                                  setState(() {
+                                    isUserLogin = true;
+                                  });
+                                },
+                                child: Column(
+                                  children: [
+                                    Text(
+                                      "User Login",
+                                      style: TextStyle(
+                                        color:
+                                            isUserLogin
+                                                ? Color.fromARGB(
+                                                  255,
+                                                  88,
+                                                  13,
+                                                  218,
+                                                )
+                                                : Colors.black,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16,
+                                      ),
+                                    ),
+                                    SizedBox(height: 10),
+                                    Container(
+                                      height: 3,
+                                      color:
+                                          isUserLogin
+                                              ? Color.fromARGB(255, 88, 13, 218)
+                                              : Colors.transparent,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: GestureDetector(
+                                onTap: () {
+                                  setState(() {
+                                    isUserLogin = false;
+                                  });
+                                },
+                                child: Column(
+                                  children: [
+                                    Text(
+                                      "Admin Login",
+                                      style: TextStyle(
+                                        color:
+                                            !isUserLogin
+                                                ? Color.fromARGB(
+                                                  255,
+                                                  88,
+                                                  13,
+                                                  218,
+                                                )
+                                                : Colors.black,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16,
+                                      ),
+                                    ),
+                                    SizedBox(height: 10),
+                                    Container(
+                                      height: 3,
+                                      color:
+                                          !isUserLogin
+                                              ? Color.fromARGB(255, 88, 13, 218)
+                                              : Colors.transparent,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 30),
+
+                        // Conditional form fields based on selected tab
+                        if (isUserLogin) ...[
+                          // User Login Form
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              SizedBox(height: 8),
+                              TextField(
+                                decoration: InputDecoration(
+                                  hintText: "Enter your Student ID",
+                                  hintStyle: TextStyle(color: Colors.grey),
+                                  enabledBorder: UnderlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color: Color.fromARGB(255, 88, 13, 218),
+                                    ),
+                                  ),
+                                  focusedBorder: UnderlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color: Color.fromARGB(255, 88, 13, 218),
+                                    ),
+                                  ),
+                                  labelText: "Student ID",
+                                  labelStyle: TextStyle(color: Colors.grey),
+                                  floatingLabelStyle: TextStyle(
+                                    color: Color.fromARGB(255, 88, 13, 218),
+                                  ),
+                                ),
+                              ),
+
+                              SizedBox(height: 30),
+                              Center(
+                                child: ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor: Color.fromARGB(
+                                      255,
+                                      88,
+                                      13,
+                                      218,
+                                    ),
+                                    minimumSize: Size(double.infinity, 50),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(8),
+                                    ),
+                                  ),
+                                  onPressed: () {},
+                                  child: Text(
+                                    "Login",
+                                    style: GoogleFonts.inter(
+                                      fontSize: 18,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ] else ...[
+                          // Admin Login Form
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              SizedBox(height: 8),
+                              TextField(
+                                decoration: InputDecoration(
+                                  hintText: "Enter your username",
+                                  hintStyle: TextStyle(color: Colors.grey),
+                                  enabledBorder: UnderlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color: Color.fromARGB(255, 88, 13, 218),
+                                    ),
+                                  ),
+                                  focusedBorder: UnderlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color: Color.fromARGB(255, 88, 13, 218),
+                                    ),
+                                  ),
+                                  labelText: "Username",
+                                  labelStyle: TextStyle(color: Colors.grey),
+                                  floatingLabelStyle: TextStyle(
+                                    color: Color.fromARGB(255, 88, 13, 218),
+                                  ),
+                                ),
+                              ),
+                              SizedBox(height: 20),
+                              TextField(
+                                obscureText: true,
+                                decoration: InputDecoration(
+                                  hintText: "••••••••••••••",
+                                  hintStyle: TextStyle(color: Colors.grey),
+                                  enabledBorder: UnderlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color: Color.fromARGB(255, 88, 13, 218),
+                                    ),
+                                  ),
+                                  focusedBorder: UnderlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color: Color.fromARGB(255, 88, 13, 218),
+                                    ),
+                                  ),
+                                  labelText: "Password",
+                                  labelStyle: TextStyle(color: Colors.grey),
+                                  floatingLabelStyle: TextStyle(
+                                    color: Color.fromARGB(255, 88, 13, 218),
+                                  ),
+                                ),
+                              ),
+                              SizedBox(height: 30),
+                              Center(
+                                child: ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor: Color.fromARGB(
+                                      255,
+                                      88,
+                                      13,
+                                      218,
+                                    ),
+                                    minimumSize: Size(double.infinity, 50),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(8),
+                                    ),
+                                  ),
+                                  onPressed: () {},
+                                  child: Text(
+                                    "Login",
+                                    style: GoogleFonts.inter(
+                                      fontSize: 18,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+
+                        // Footer
+                        Spacer(),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "Made with ",
+                              style: GoogleFonts.inter(fontSize: 12),
+                            ),
+                            Icon(Icons.favorite, color: Colors.red, size: 12),
+                            Text(
+                              " by ",
+                              style: GoogleFonts.inter(fontSize: 12),
+                            ),
+                            Text(
+                              "MarsLab",
+                              style: GoogleFonts.inter(
+                                color: Color.fromARGB(255, 88, 13, 218),
+                                fontSize: 12,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+      ),
+    );
+  }
+
+  Widget _buildSidebar(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [
+          // Purple header with app description
+          Container(
+            width: double.infinity,
+            padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 50),
+            color: const Color.fromARGB(255, 88, 13, 218),
+            child: Text(
+              '\n\n\n\n\n\nDIU Route Explorers is a university bus schedule app that allows students to check bus routes, start and departure times, and important notes for a smooth commuting experience.',
+              style: GoogleFonts.inter(
+                color: Colors.white,
+                fontSize: 16,
+                height: 1.6,
+                letterSpacing: 0.2,
+              ),
+            ),
+          ),
+
+          // Menu items with improved spacing
+          SizedBox(height: 10),
+
+          ListTile(
+            contentPadding: EdgeInsets.symmetric(horizontal: 24, vertical: 8),
+            leading: Icon(Icons.schedule),
+            title: Text('Bus Schedule', style: GoogleFonts.inter(fontSize: 16)),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+
+          Divider(height: 1, thickness: 0.5),
+
+          ListTile(
+            contentPadding: EdgeInsets.symmetric(horizontal: 24, vertical: 8),
+            leading: Icon(Icons.route),
+            title: Text(
+              'Route Information',
+              style: GoogleFonts.inter(fontSize: 16),
+            ),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+
+          Divider(height: 1, thickness: 0.5),
+
+          ListTile(
+            contentPadding: EdgeInsets.symmetric(horizontal: 24, vertical: 8),
+            leading: Icon(Icons.notifications),
+            title: Text(
+              'Notifications',
+              style: GoogleFonts.inter(fontSize: 16),
+            ),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+
+          Divider(height: 1, thickness: 0.5),
+
+          ListTile(
+            contentPadding: EdgeInsets.symmetric(horizontal: 24, vertical: 8),
+            leading: Icon(Icons.settings),
+            title: Text('Settings', style: GoogleFonts.inter(fontSize: 16)),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+
+          Divider(height: 1, thickness: 0.5),
+
+          ListTile(
+            contentPadding: EdgeInsets.symmetric(horizontal: 24, vertical: 8),
+            leading: Icon(Icons.help),
+            title: Text(
+              'Help and Support',
+              style: GoogleFonts.inter(fontSize: 16),
+            ),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+
+          Divider(height: 1, thickness: 0.5),
+
+          // Logout at the bottom with red text
+          Spacer(),
+          Divider(height: 1, thickness: 0.5),
+
+          ListTile(
+            contentPadding: EdgeInsets.symmetric(horizontal: 24, vertical: 8),
+            leading: Icon(Icons.logout, color: Colors.red),
+            title: Text(
+              'Logout',
+              style: GoogleFonts.inter(color: Colors.red, fontSize: 16),
+            ),
+            trailing: Icon(Icons.arrow_forward, color: Colors.red),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+
+          // Version and footer
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20.0),
+            child: Column(
+              children: [
+                Text(
+                  'Version 1.0.1',
+                  style: GoogleFonts.inter(color: Colors.grey, fontSize: 12),
+                ),
+                SizedBox(height: 6),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Made with ", style: GoogleFonts.inter(fontSize: 12)),
+                    Icon(Icons.favorite, color: Colors.red, size: 12),
+                    Text(" by ", style: GoogleFonts.inter(fontSize: 12)),
+                    Text(
+                      "MarsLab",
+                      style: GoogleFonts.inter(
+                        color: Color.fromARGB(255, 88, 13, 218),
+                        fontSize: 12,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
